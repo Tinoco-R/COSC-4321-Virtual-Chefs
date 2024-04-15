0d103bb2b8807c045113e6943bb53df817a94c59
Woosung Nam <wnam@cougarnet.uh.edu>
2024-04-15 18:22:03 -0500

    fix: isgrabbed true after first grab feat: add more steps for tutorial

--- VirtualChefs/Assets/Scripts/tutorial/tutorial.cs
+++ VirtualChefs/Assets/Scripts/tutorial/tutorial.cs
@@ -51,10 +51,24 @@ public class Tutorial : MonoBehaviour
                 PlaySound(CompleteSound);
                 break;
             case 5:
-                text.text = "Good job! You've grabbed the plate. Tutorial completed!";
+                text.text = "Good job! You've grabbed the plate. Now cut the lettuce and place on the plate";
+                PlaySound(CompleteSound);
+                
+                break;
+            case 6:
+                text.text = "Good job! You've placed the lettuce on the plate. Now cook the meat and place on the plate";
+                PlaySound(CompleteSound);
+                break;
+            case 7:
+                text.text = "Good job! You've placed the meat on the plate. Now turn it in";
+                PlaySound(CompleteSound);
+                break;
+            case 8:
+                text.text = "Good job! You've turned in the order! Tutorial Completed!";
                 PlaySound(CompleteSound);
                 tutorialCompleted = true;
                 break;
+            
         }
     }
 
@@ -86,9 +100,27 @@ public class Tutorial : MonoBehaviour
                     currentStage = 4;
                     UpdateTutorialText();
                 }
-                else if (CheckGrabbable(grabbedObject, "Plate", 4))
+                
+                
+                else if (CheckGrabbable(grabbedObject,"Plate" , 4))
                 {
                     currentStage = 5;
+                    UpdateTutorialText();
+                }
+                
+                else if (CheckGrabbable(grabbedObject, "LettuceSlice", 5))
+                {
+                    currentStage = 6;
+                    UpdateTutorialText();
+                }
+                else if (CheckGrabbable(grabbedObject, "CookedMeat", 6))
+                {
+                    currentStage = 7;
+                    UpdateTutorialText();
+                }
+                else if (CheckTurnInZoneScore())
+                {
+                    currentStage = 8;
                     UpdateTutorialText();
                 }
             }
@@ -114,6 +146,27 @@ public class Tutorial : MonoBehaviour
         }
         return false;
     }
+    private bool CheckTurnInZoneScore()
+    {
+        // Find the TurnInZone prefab instance by its tag
+        GameObject turnInZoneInstance = GameObject.FindGameObjectWithTag("TurnInZone");
+
+        if (turnInZoneInstance != null)
+        {
+            // Get the ReadFood component from the TurnInZone instance
+            ReadFood readFood = turnInZoneInstance.GetComponent<ReadFood>();
+
+            if (readFood.score != 0)
+            {
+                // The score is not 0, so return true
+                return true;
+            }
+        }
+
+        // The TurnInZone instance or its ReadFood component was not found, or the score is 0
+        return false;
+    }
+
     
     private void PlaySound(AudioClip clip)
     {
